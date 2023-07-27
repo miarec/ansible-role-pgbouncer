1ffa165bb1f3f95043e7b9f0127798be99158e2e
Timothy Lytle <timothy.lytle@miarec.com>
2023-07-27 16:17:51 -0600

    added idepotence to pgbouncer.yml

--- molecule/default/tests/test_defatuls.py
+++ molecule/default/tests/test_defatuls.py
@@ -5,21 +5,25 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-# def test_directories(host):
-#     dirs = [
-#         "/usr/local/bin/python3.6",
-#         "/etc/grafana/dashboard",
-#         "/etc/grafana/provisioning/dashboards/"
-#     ]
-#     for dir in dirs:
-#         d = host.file(dir)
-#         assert d.is_directory
-#         assert d.exists
+def test_directories(host):
+    dirs = [
+        "/etc/pgbouncer",
+        "/var/log/pgbouncer",
+        "/var/run/pgbouncer"
+    ]
+    for dir in dirs:
+        d = host.file(dir)
+        assert d.is_directory
+        assert d.exists
 
 
 def test_files(host):
     files = [
-        "/usr/local/bin/python3.8"
+        "/etc/pgbouncer/pgbouncer.ini",
+        "etc/pgbouncer/userlist.txt",
+        "/etc/pgbouncer/pgbouncer_hba.conf",
+        "/var/log/pgbouncer/pgbouncer.log",
+        "/var/run/pgbouncer/pgbouncer.pid"
     ]
     for file in files:
         f = host.file(file)
@@ -27,16 +31,16 @@ def test_files(host):
         assert f.is_file
 
 
-# def test_service(host):
-#     s = host.service("grafana-server")
-#     assert s.is_enabled
-#     assert s.is_running
+def test_service(host):
+    s = host.service("pgbouncer")
+    assert s.is_enabled
+    assert s.is_running
 
 
-# def test_socket(host):
-#     sockets = [
-#         "tcp://0.0.0.0:3000"
-#     ]
-#     for socket in sockets:
-#         s = host.socket(socket)
-#         assert s.is_listening
+def test_socket(host):
+    sockets = [
+        "tcp://0.0.0.0:6432"
+    ]
+    for socket in sockets:
+        s = host.socket(socket)
+        assert s.is_listening
